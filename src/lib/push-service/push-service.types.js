90829c56cbf7e99e7ea911d06a1558876a8d6e67
biancalui-emarsys <bianca.lui@emarsys.com>
2023-10-18 15:08:11 +0800

    test local sdk

--- src/lib/push-service/push-service.types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IPushService = void 0;
-class IPushService {
-}
-exports.IPushService = IPushService;
-//# sourceMappingURL=push-service.types.js.map
