dd3c36fb54a5bc7c401b5dd750c6a1133a732143
biancalui-emarsys <bianca.lui@emarsys.com>
2024-01-03 11:33:58 +0800

    test staging sdk

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.importScripts("/src/web-emarsys-service-worker.js")
+self.importScripts("https://redirector-staging.eservice.emarsys.com/web-emarsys-sdk-v4/latest/web-emarsys-service-worker.js")
