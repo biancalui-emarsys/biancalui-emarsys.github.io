d39c6de8b8817cf8250a6df2d645795ff187e1a2
biancalui-emarsys <bianca.lui@emarsys.com>
2024-01-09 12:54:31 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.importScripts("https://redirector-staging.eservice.emarsys.com/web-emarsys-sdk-v4/latest/web-emarsys-service-worker.js")
+self.importScripts("https://client-version-staging.cf.emarsys.com/web-emarsys-sdk-v4/latest/web-emarsys-service-worker.js")
